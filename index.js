e1670342c1deffedc2647ac2dc327e00cadc9828
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-05-08 14:14:16 +0700

    Fix cannot connect to visits web app

--- index.js
+++ index.js
@@ -2,19 +2,32 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient({
-  host: 'redis-server',
-  port: 6379,
-});
-client.set('visits', 0);
 
-app.get('/', (req, res) => {
-  client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits);
-    client.set('visits', parseInt(visits) + 1);
-  });
+const client = redis.createClient({
+    url: 'redis://redis-server:6379'
+});
+
+// Bắt lỗi kết nối nếu có
+client.on('error', err => console.log('Redis Client Error', err));
+
+client.connect()
+    .then(() => {
+        client.set('visits', 0);
+    })
+    .catch(console.error);
+
+app.get('/', async (req, res) => {
+    try {
+        const visits = await client.get('visits');
+        res.send('Number of visits is ' + visits);
+        
+        await client.set('visits', parseInt(visits) + 1);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Something went wrong!');
+    }
 });
 
 app.listen(8081, () => {
-  console.log('Listening on port 8081');
+    console.log('Listening on port 8081');
 });
